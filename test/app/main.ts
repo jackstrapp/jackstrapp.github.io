13331331056183ef4becbdfd254f3259439a8d97
jackstrapp <sherman91strike@hotmail.fr>
2016-10-13 17:12:01 +0200

    Update main.ts

--- test/app/main.ts
+++ test/app/main.ts
@@ -4,7 +4,13 @@ import {DBService} from './model/Database';
 import { CompteurService } from './Services/CompteurService';
 import { ReleveService } from './Services/ReleveService';
 import { AppModule } from './app.module';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule, [
+  ROUTER_PROVIDERS,
+  {provide: LocationStrategy, useClass: HashLocationStrategy}
+]);
+
 
