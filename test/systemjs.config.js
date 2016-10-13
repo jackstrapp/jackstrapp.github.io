916865942ee5a9893fb1cd92f9f820332502642d
jackstrapp <sherman91strike@hotmail.fr>
2016-10-13 17:01:31 +0200

    Add files via upload

--- test/systemjs.config.js
+++ test/systemjs.config.js
@@ -23,7 +23,6 @@
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
-      '@bootstrap'
 
       // other libraries
       'rxjs':                      'npm:rxjs',
