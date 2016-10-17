ba8704591f4ab25dab758765730755f14210fc53
jackstrapp <sherman91strike@hotmail.fr>
2016-10-17 10:19:10 +0200

    Add files via upload

---
+++ test/app/main/main.menu.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+import Dexie from "dexie";
+import * as Rx from 'rxjs/Rx';
+
+
+@Component({
+	moduleId: module.id,
+	template: `
+		
+	`
+})
+export class MainMenuComponent {
+	constructor(){
+	}
+
+}
