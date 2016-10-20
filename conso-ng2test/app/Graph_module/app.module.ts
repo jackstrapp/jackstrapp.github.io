99a4473c40a465e293a1db9427601be190c9bc1b
Jonathan RENE <jrene@AUSYNET.FR>
2016-10-20 10:31:06 +0200

    ...

--- conso-ng2test/app/Graph_module/app.module.ts
+++ conso-ng2test/app/Graph_module/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app.routes';
 
-import { GraphComponent } from './components/Graph/graph.component';
+import { GraphComponent } from './components/graph/graph.component';
 
 
 @NgModule({
